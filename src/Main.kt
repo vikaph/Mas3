9ba83be39c2e94606f5470436865ba0b81314472
357-22 <vika.p.2006@bk.ru>
2024-10-26 11:39:19 +0300

    Готовое решение

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val alphabet = charArrayOf(
+        'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+        'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т',
+        'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+        'Э', 'Ю', 'Я')
+//Ассоциация символов с их позициями
+    val positions = alphabet.indices.associate { alphabet[it] to it + 1 }
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    println("Выберите действие: зашифровать (1) или расшифровать (2):")
+    val action = readLine()
+
+    println("Введите ключевое слово:")
+    val keyword = readLine()!!.uppercase()
+
+    println("Введите текст:")
+    val text = readLine()!!.uppercase()
+
+    val result = when (action) {
+        "1" -> shifr(text, keyword, positions)
+        "2" -> deshifr(text, keyword, positions)
+        else -> "Неверный выбор действия."
     }
+    println("Результат: $result")
+}
+
+fun shifr(text: String, keyword: String, positions: Map<Char, Int>): String {
+    val shiftedText = StringBuilder()
+    val keywordLength = keyword.length
+
+    for ((index, char) in text.withIndex()) {
+        if (positions.containsKey(char)) { // Проверка, является ли символ буквой алфавита
+            val shift = positions[keyword[index % keywordLength]]!! //сдвиг относительно ключевого слова
+            val originalPosition = positions[char]!!
+            val newPosition = (originalPosition + shift - 1) % 33 + 1
+            shiftedText.append(positions.entries.first { it.value == newPosition }.key)
+        } else {
+            shiftedText.append(char)
+        }
+    }
+    return shiftedText.toString()
+}
+
+fun deshifr(text: String, keyword: String, positions: Map<Char, Int>): String {
+    val shiftedText = StringBuilder()
+    val keywordLength = keyword.length
+
+    for ((index, char) in text.withIndex()) {
+        if (positions.containsKey(char)) { //проверка
+            val shift = positions[keyword[index % keywordLength]]!! //сдвиг
+            val originalPosition = positions[char]!!
+            val newPosition = (originalPosition - shift - 1 + 33) % 33 + 1
+            shiftedText.append(positions.entries.first { it.value == newPosition }.key)
+        } else {
+            shiftedText.append(char)
+        }
+    }
+    return shiftedText.toString()
 }
